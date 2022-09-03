27b63d57837d26b6c3125cfa4f1bd142710c9fa3
JSanjaya <john.sanjaya999@gmail.com>
2022-09-02 21:02:04 -0700

    cors update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,7 +12,7 @@ const {
   } = require("../authenticate")
 
   router.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://medicheck-in.netlify.app/");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
    });
